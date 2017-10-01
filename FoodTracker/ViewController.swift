4616bcf0e33cf9bc462a3c8bd957d2c76ca47741
LeoHeo <hjh5488@gmail.com>
2017-10-01 16:06:44 +0900

    Init FoodTracker with imageView

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,18 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+  
+  @IBOutlet weak var nameTextField: UITextField!
+  @IBOutlet weak var mealNameLabel: UILabel!
 
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    print("viewWillAppear \(animated)")
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    print("viewDidAppear \(animated)")
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    print("viewWillDisappear \(animated)")
+  }
+  
+  override func viewDidDisappear(_ animated: Bool) {
+    super.viewDidDisappear(animated)
+    print("viewDidDisappear \(animated)")
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    print("viewDidLoad")
+    nameTextField.delegate = self
     // Do any additional setup after loading the view, typically from a nib.
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    // Hide the keyboard
+    print("should Return")
+    textField.resignFirstResponder()
+    return true
   }
-
+  
+  func textFieldDidEndEditing(_ textField: UITextField) {
+    print("End Editing")
+    mealNameLabel.text = textField.text
+  }
+  
+  @IBAction func setDefaultLabelText(_ sender: UIButton) {
+    mealNameLabel.text = "Default Text"
+  }
 
 }
 
