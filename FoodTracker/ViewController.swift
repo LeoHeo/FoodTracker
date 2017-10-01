b741095e47fdc204fd4eb23b286ecad762959a4f
LeoHeo <hjh5488@gmail.com>
2017-10-01 21:19:05 +0900

    Added photoLibrary permission
    - 이미지뷰에 사진 불러오기 위해 권한 설정
    - 이미지뷰에 사진 불러오기

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate,
+  UIImagePickerControllerDelegate, UINavigationControllerDelegate {
   
   @IBOutlet weak var nameTextField: UITextField!
   @IBOutlet weak var mealNameLabel: UILabel!
-
+  @IBOutlet weak var photoImageView: UIImageView!
+  
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
     print("viewWillAppear \(animated)")
@@ -55,6 +57,44 @@ class ViewController: UIViewController, UITextFieldDelegate {
   @IBAction func setDefaultLabelText(_ sender: UIButton) {
     mealNameLabel.text = "Default Text"
   }
-
+  
+  @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+    
+    // Hide the keyboard
+    nameTextField.resignFirstResponder()
+    
+    // UIImagePickerController is a view controller that lets a user pick media from their photo Library
+    let imagePickerController = UIImagePickerController()
+    
+    // Only allow to be pikced, not taken
+    imagePickerController.sourceType = .photoLibrary
+    
+    // Make sure ViewController notified when the user picks an image
+    imagePickerController.delegate = self
+    
+    present(imagePickerController, animated: true, completion: nil)
+  }
+  
+  func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+    print("imagePickerControllerDidCancel")
+    
+    // Dismiss the picker if the user canceled
+    // Photo Library에서 cancel을 눌렀을때
+    dismiss(animated: true, completion: nil)
+  }
+  
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+      fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+    }
+    
+    print("selected image")
+    
+    // Set photoImageView to display the selected image
+    photoImageView.image = selectedImage
+    
+    // Dismiss the picker
+    dismiss(animated: true, completion: nil)
+  }
 }
 
